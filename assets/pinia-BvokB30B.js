96502f1b929e1b29b1f1bf7183f1ce817d6596a3
p.stachura <patryk.stachura@currenda.pl>
2025-07-23 18:44:14 +0200

    Updates

---
+++ assets/pinia-BvokB30B.js
@@ -0,0 +1,1 @@
+import{c as n}from"./index-C7K7vZ2K.js";const a=({app:t})=>{const i=n();t.use(i)};export{a as default};
--- assets/pinia-CEOx9Uh7.js
+++
@@ -1,1 +0,0 @@
-import{c as n}from"./index-D9t-kWGX.js";const a=({app:t})=>{const i=n();t.use(i)};export{a as default};

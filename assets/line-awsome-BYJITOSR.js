9338af9d7344577cfb475fdb62ac395071986b06
p.stachura <patryk.stachura@currenda.pl>
2025-07-30 18:19:39 +0200

    Updates

--- assets/line-awsome-BYJITOSR.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/index-DWBghgLE.js","assets/index-Br300Yc9.css"])))=>i.map(i=>d[i]);
-import{_ as i}from"./index-DWBghgLE.js";import{b as e}from"./index-3fevySbJ.js";import{l as t}from"./log.serivce-Cy1HIKeW.js";const s=e(async()=>{await i(()=>import("./index-DWBghgLE.js").then(o=>o.ag),__vite__mapDeps([0,1])),t.info("Line Awsome initialized.")});export{s as default};
